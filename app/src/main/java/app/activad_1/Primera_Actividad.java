10a37a9f89b268c920b68f63f7cf403a8256c02f
edwin <emgonzalezm@ufpso.edu.co>
2017-03-29 21:57:34 -0500

    Initial commit

---
+++ app/src/main/java/app/activad_1/Primera_Actividad.java
@@ -0,0 +1,13 @@
+package app.activad_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Primera_Actividad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primera__actividad);
+    }
+}
